61871b341a05e36846a51089e70da9080cc7a130
ArturAlvess <contatopessoal997@gmail.com>
2023-05-14 20:30:20 -0300

    responsive finalizado

--- stats-screen/js/main.js
+++ stats-screen/js/main.js
@@ -1,3 +1,11 @@
+'use strict'
+
+const exit = () => {
+    const buttonSair = document.querySelector('.button-leave')
+    buttonSair.onclick = function (){
+        window.location.href = 'http://127.0.0.1:5500/'
+    }
+}
 
 
-
+exit();
